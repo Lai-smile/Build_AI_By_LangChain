a64cca8883cd56d05f4466737978734df650d041
Jason Lai <58462166+Lai-smile@users.noreply.github.com>
2025-12-12 15:04:37 +0800

    Create api_keys.py
    Here, Stores various keys.

---
+++ api_keys.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+# Time: 2024/12/12 14:59
+# FileName: exts
+# Project: LangchainStudy
+# Author: JasonLai
+# Email: jasonlaihj@163.com
+"""
+Here, Stores various keys.
+"""
+
+# QWen
+ty_api_key = "your qwen api key"
+
+# openrouter
+openrouter_api_key = "your openrouter api key"
